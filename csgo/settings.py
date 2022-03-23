0f0ae5d9bd537e189f0b2109d289c9c017dcda17
Эрхаз Кочкаров <ekochkarov2023@LI2.local>
2022-03-23 16:00:52 +0300

    task completed

--- csgo/settings.py
+++ csgo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'csgo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['csgoteamname/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('teamnames/404', views.getNotFound),
+    path('teamnames/',views.getSport),
+    path('teamnames/<name>', views.getTeamname),
+
 ]
